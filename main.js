f1856c298214d3f1a4ecc88c057664ef666aba79
Gurdas Satwani <gurdassatwani1@gmail.com>
2025-11-19 20:19:32 +0530

    Backing it up

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
-// /^(\d+)[+-/*](\d+)$/
-// /^-?(\d+(\.\d+)?)[+-/*]-?(\d+(\.\d+)?)$/
+"use strict";
 
-const exp1 = "(+ 2 5 6)"; // (2 + 5) + 6 = 13
-const exp2 = "(- 2 5 6)"; // (2 - 5) - 6 = -9
-const exp3 = "(* 2 5 6)"; // (2 * 5) * 6 = 60
-const exp4 = "(/ 2 5 6)"; // (2 / 5) / 6 ≈ 0.67
-// Left -> Right associativity
-// Inside -> Outside
-// Prefix notation and their is no need for operator precedence
-// Checkout SICP for implementing parser
-// after evaluating each expression the parser should return a "rational" object
-// I need to use recursion and LIFO data structure for this
+const foo = function () {
+  const inputField = document.createElement("div");
 
-function foo(exp) {
-  let i = 0;
-  while (i < exp.length) {
-    console.log(exp[i++]);
-  }
-}
+  const expressionTextArea = document.createElement("textarea");
+  expressionTextArea.setAttribute("name", "hhhh");
+  expressionTextArea.setAttribute("id", "hhhh");
 
-foo(exp1);
+  const resultTextField = document.createElement("input");
+
+  inputField.append(expressionTextArea);
+  inputField.append(resultTextField);
+  return inputField;
+};
+
+const mainContainer = document.querySelector(".main__container");
+
+// mainContainer.append(foo());
